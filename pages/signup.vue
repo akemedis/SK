5a3660cfc5cea199c6ad977ee2f44c2a34cd1c3d
akemedis <41940887+akemedis@users.noreply.github.com>
2023-01-07 19:29:28 +1100

    1 binaries uploaded

--- pages/signup.vue
+++ pages/signup.vue
@@ -1,195 +1,193 @@
 <template>
-  <div
-    class="flex justify-center"
-    style="background-image: url(../assets/images/fish_concept.png)"
-  >
-    <div
-      class="
-        flex
-        justify-center
-        mt-10
-        bg-gradient-to-b
-        from-purple-600
-        via-blue-700
-        to-cyan-600
-        max-w-fit
-        rounded-2xl
-        shadow-xl shadow-gray-300
-        hover:shadow-2xl hover:shadow-blue-200
-        duration-200
-      "
-    >
-      <form
+  <div id="back" class="min-h-screen">
+    <div class="flex justify-center">
+      <div
         class="
-          flex flex-wrap
-          justify-center justify-self-center
-          border
-          m-1
-          p-7
+          flex
+          justify-center
+          mt-10
+          bg-gradient-to-b
+          from-purple-600
+          via-blue-700
+          to-cyan-600
+          max-w-fit
           rounded-2xl
-          max-w-sm
-          bg-gray-100
+          shadow-md shadow-gray-300
+          hover:shadow-2xl hover:shadow-blue-200
+          duration-200
         "
       >
-        <div
+        <form
           class="
-            block
-            p-2
-            mb-8
-            font-anu font-medium
-            text-2xl
-            bg-gradient-to-r
-            from-purple-600
-            via-blue-700
-            to-cyan-600
-            bg-clip-text
+            flex flex-wrap
+            justify-center justify-self-center
+            border
+            m-1
+            p-7
+            rounded-2xl
+            max-w-sm
+            bg-gray-100
           "
         >
-          <h1 class="text-transparent">SHAPE &nbsp THE &nbsp FUTURE</h1>
-        </div>
-        <div class="md:flex md:items-center mb-6">
-          <div class="md:w-1/3">
-            <label
-              class="
-                block
-                text-gray-500
-                font-bold
-                md:text-right
-                mb-1
-                md:mb-0
-                pr-4
-              "
-              for="inline-full-name"
-              placeholder="fullname"
-            >
-              Full Name
-            </label>
-          </div>
-          <div class="md:w-2/3">
-            <input
-              class="
-                bg-gray-200
-                appearance-none
-                border-2 border-gray-200
-                rounded
-                w-full
-                py-2
-                px-4
-                text-gray-700
-                leading-tight
-                focus:outline-none focus:bg-white focus:border-blue-500
-                font-roboto font-medium
-              "
-              id="inline-full-name"
-              type="text"
-              placeholder="Jane Doe"
-            />
-          </div>
-        </div>
-        <div class="md:flex md:items-center mb-6">
-          <div class="md:w-1/3">
-            <label
-              class="
-                block
-                text-gray-500
-                font-bold
-                md:text-right
-                mb-1
-                md:mb-0
-                pr-4
-              "
-              for="inline-full-name"
-              placeholder="fullname"
-            >
-              Email
-            </label>
-          </div>
-          <div class="md:w-2/3">
-            <input
-              class="
-                bg-gray-200
-                appearance-none
-                border-2 border-gray-200
-                rounded
-                w-full
-                py-2
-                px-4
-                text-gray-700
-                leading-tight
-                focus:outline-none focus:bg-white focus:border-blue-500
-                font-roboto font-medium
-              "
-              id="inline-full-name"
-              type="text"
-              placeholder="janedoe@gmail.com"
-            />
-          </div>
-        </div>
-        <div class="md:flex md:items-center mb-6">
-          <div class="md:w-1/3">
-            <label
-              class="
-                block
-                text-gray-500
-                font-bold
-                md:text-right
-                mb-1
-                md:mb-0
-                pr-4
-              "
-              for="inline-password"
-            >
-              Password
-            </label>
-          </div>
-          <div class="md:w-2/3">
-            <input
-              class="
-                bg-gray-200
-                appearance-none
-                border-2 border-gray-200
-                rounded
-                w-full
-                py-2
-                px-4
-                text-gray-700
-                leading-tight
-                focus:outline-none focus:bg-white focus:border-blue-500
-              "
-              id="inline-password"
-              type="password"
-              placeholder="******************"
-            />
-          </div>
-        </div>
-        <div
-          class="
-            block
-            border border-2 border-gray-300
-            rounded-lg
-            p-1
-            font-technica
-            shadow-md shadow-gray-200
-            m-8
-            mt-4
-            mb-4
-            hover:scale-110
-            hover:shadow-xl
-            hover:shadow-blue-200
-            hover:border-blue-400
-            duration-200
-          "
-        >
-          <button
-            class="decoration-red-500 duration-1000 p-1 hover:animate-pulse"
+          <div
+            class="
+              block
+              p-2
+              mb-8
+              font-anu font-medium
+              text-2xl
+              bg-gradient-to-r
+              from-purple-600
+              via-blue-700
+              to-cyan-600
+              bg-clip-text
+            "
           >
-            Join the ride
-          </button>
-        </div>
-      </form>
+            <h1 class="text-transparent">SHAPE &nbsp THE &nbsp FUTURE</h1>
+          </div>
+          <div class="md:flex md:items-center mb-6">
+            <div class="md:w-1/3">
+              <label
+                class="
+                  block
+                  text-gray-500
+                  font-bold
+                  md:text-right
+                  mb-1
+                  md:mb-0
+                  pr-4
+                "
+                for="inline-full-name"
+                placeholder="fullname"
+              >
+                Full Name
+              </label>
+            </div>
+            <div class="md:w-2/3">
+              <input
+                class="
+                  bg-gray-200
+                  appearance-none
+                  border-2 border-gray-200
+                  rounded
+                  w-full
+                  py-2
+                  px-4
+                  text-gray-700
+                  leading-tight
+                  focus:outline-none focus:bg-white focus:border-blue-500
+                  font-roboto font-medium
+                "
+                id="inline-full-name"
+                type="text"
+                placeholder="Jane Doe"
+              />
+            </div>
+          </div>
+          <div class="md:flex md:items-center mb-6">
+            <div class="md:w-1/3">
+              <label
+                class="
+                  block
+                  text-gray-500
+                  font-bold
+                  md:text-right
+                  mb-1
+                  md:mb-0
+                  pr-4
+                "
+                for="inline-full-name"
+                placeholder="fullname"
+              >
+                Email
+              </label>
+            </div>
+            <div class="md:w-2/3">
+              <input
+                class="
+                  bg-gray-200
+                  appearance-none
+                  border-2 border-gray-200
+                  rounded
+                  w-full
+                  py-2
+                  px-4
+                  text-gray-700
+                  leading-tight
+                  focus:outline-none focus:bg-white focus:border-blue-500
+                  font-roboto font-medium
+                "
+                id="inline-full-name"
+                type="text"
+                placeholder="janedoe@gmail.com"
+              />
+            </div>
+          </div>
+          <div class="md:flex md:items-center mb-6">
+            <div class="md:w-1/3">
+              <label
+                class="
+                  block
+                  text-gray-500
+                  font-bold
+                  md:text-right
+                  mb-1
+                  md:mb-0
+                  pr-4
+                "
+                for="inline-password"
+              >
+                Password
+              </label>
+            </div>
+            <div class="md:w-2/3">
+              <input
+                class="
+                  bg-gray-200
+                  appearance-none
+                  border-2 border-gray-200
+                  rounded
+                  w-full
+                  py-2
+                  px-4
+                  text-gray-700
+                  leading-tight
+                  focus:outline-none focus:bg-white focus:border-blue-500
+                "
+                id="inline-password"
+                type="password"
+                placeholder="******************"
+              />
+            </div>
+          </div>
+          <div
+            class="
+              block
+              border border-2 border-gray-300
+              rounded-lg
+              p-1
+              font-technica
+              shadow-md shadow-gray-200
+              m-8
+              mt-4
+              mb-4
+              hover:scale-110
+              hover:shadow-xl
+              hover:shadow-blue-200
+              hover:border-blue-400
+              duration-200
+            "
+          >
+            <button
+              class="decoration-red-500 duration-1000 p-1 hover:animate-pulse"
+            >
+              Join the ride
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   </div>
-  <!-- <img src="../assets/images/fish_concept.png" alt="Beautiful World" /> -->
   <div class="custom-shape-divider-bottom-1673077320">
     <svg
       data-name="Layer 1"
@@ -240,5 +238,10 @@ const { data, error } = await supabase.auth.signUp({
 
 .custom-shape-divider-bottom-1673077320 .shape-fill {
   fill: #2563eb;
+}
+
+#back {
+  background-image: url(../assets/images/fish_concept.png);
+  background-size: cover;
 }
 </style>
