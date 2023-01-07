da58cd5441482607f7b1b50a2f7a78a000f5d332
akemedis <41940887+akemedis@users.noreply.github.com>
2023-01-07 22:48:52 +1100

    1 file modified

--- pages/signup.vue
+++ pages/signup.vue
@@ -186,7 +186,7 @@
         >
           <button
             class="decoration-red-500 duration-1000 p-1 hover:animate-pulse"
-            @click="log(name)"
+            @click="sign_up(name, email, password)"
             type="button"
           >
             Join the ride
@@ -222,16 +222,65 @@ const supabase = createClient(url, key);
 const name = ref(null);
 const email = ref(null);
 const password = ref(null);
+var result = null;
 
-function sign_in(ent) {
-  console.log(ent.value);
+function sign_up(name, email, password) {
+  if (name.value !== '' && email.value !== '' && password.value !== '') {
+    // console.log(email.value, name.value, password.value);
+    // signing up user
+    const signingUp = async () => {
+      const { data, error } = await supabase.auth.signUp({
+        email: email.value,
+        password: password.value,
+        raw_user_meta_data: {
+          username: name.value,
+        },
+      }).then((result) => {
+        if (result.error) {
+          alert(result.error.error_description || result.error.message);
+          return
+        } else {
+          console.log(result.data)
+          result = result.data
+          alert('I blew you over an email, check it and confirm, then you can log in');
+          return
+        }
+      }
+    }
+  } else {
+    alert('You gotta fill out ALL the fields lol');
+  }
 }
-
-// signing up user
-const { data, error } = await supabase.auth.signUp({
-  email: 'example@email.com',
-  password: 'example-password',
-});
+// function sign_up(name, email, password) {
+//   if (name.value !== '' && email.value !== '' && password.value !== '') {
+//     console.log('base');
+//     const signingUp = async () => {
+//       console.log('lvl1');
+//       try {
+//         console.log('in');
+//         const { data, error } = await supabase.auth
+//           .signUp({
+//             email: email.value,
+//             password: password.value,
+//           })
+//           .then((result) => {
+//             if (result.error) {
+//               alert(error.error_description || error.message);
+//             } else {
+//               console.log(result.data);
+//               alert(
+//                 'I blew you over an email, check it and confirm, then you can log in'
+//               );
+//             }
+//           });
+//       } catch (error) {
+//         alert(error.message);
+//       }
+//     };
+//   } else {
+//     alert('You gotta fill out ALL the fields lol');
+//   }
+// }
 </script>
 
 <style>
