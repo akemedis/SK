84343c29f74d5903258d6f47ee57b6f3bda014a4
akemedis <41940887+akemedis@users.noreply.github.com>
2023-01-10 08:40:19 +1100

    1 file modified

--- components/customNav.vue
+++ components/customNav.vue
@@ -11,12 +11,21 @@
           justify-center
         "
       >
-        <v-typical
-          class="blink text-white font-anu text-2xl text-white-100"
-          :steps="['', 200, 'ENVISION', 500, 'BUILD', 500, 'HOLY SHIT', 1000]"
-          :loop="infinity"
-          :wrapper="'h2'"
-        ></v-typical>
+        <client-only placeholder="">
+          <v-typical
+            class="blink text-white font-anu text-2xl text-white-100"
+            :steps="[
+              '',
+              200,
+              'FOR A BETTER FUTURE',
+              500,
+              'SERAJ KOTAGAMA',
+              1000,
+            ]"
+            :loop="1"
+            :wrapper="'h2'"
+          ></v-typical>
+        </client-only>
         <navbutton :title="'Thoughts'" :link="'Thoughts'" />
         <navbutton :title="'Essays'" :link="'Essays'" />
         <!-- <navbutton :title="'Visual Diary'" :link="'VisualDiary'" /> -->
