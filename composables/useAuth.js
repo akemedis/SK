e37f775581794c16c206f4376c65b7f7fc7c7299
akemedis <41940887+akemedis@users.noreply.github.com>
2023-01-12 18:15:21 +1100

    1 file modified

--- composables/useAuth.js
+++ composables/useAuth.js
@@ -1,16 +1,33 @@
+import { useAuthStore } from '@/store/authStore';
+
 export const useAuth = (supabase) => {
   const get_user = async (supabase) => {
-    const {
-      data: { user },
-    } = await supabase.auth.getUser();
-    console.log(user);
+    try {
+      const {
+        data: { user },
+      } = await supabase.auth.getUser();
+      const authStore = useAuthStore();
+      console.log(
+        user,
+        JSON.parse(JSON.stringify(authStore.user)) // this is how you unpack the proxy
+      );
+      return {
+        root_user: user,
+        stored_user: JSON.parse(JSON.stringify(authStore.user)),
+      };
+    } catch (error) {
+      console.error('Error fetching user:', error);
+    }
   };
   const log_out = async (supabase) => {
     const { error } = await supabase.auth.signOut();
     console.log(error);
     if (!error) {
+      const authStore = useAuthStore();
+      authStore.user = {};
       console.log('logged out');
     }
+    return True;
   };
   const signInWithEmail = async (email, password, supabase) => {
     try {
@@ -18,7 +35,14 @@ export const useAuth = (supabase) => {
         email,
         password,
       });
-      console.log('Logged in user:', data);
+      // updating store
+      const authStore = useAuthStore();
+      authStore.user = data;
+      console.log('Logged in user:', data, authStore.user);
+      return {
+        stored_user: JSON.parse(JSON.stringify(authStore.user)),
+        root_user: data,
+      };
     } catch (error) {
       console.error('Error logging in:', error);
     }
